d14afc0b9970a3cbeae865878787ae9e5f29bc28
Kacper-Bal <kacperbalcerek2006@gmail.com>
2025-11-15 16:45:28 +0100

    1

--- information.php
+++ information.php
@@ -23,28 +23,30 @@ $resultGames = $resultGames -> fetch_row();
 <body>
     <?php include('header.php'); ?>
 
-    <div class="about_container">
-        <div class="about_content">
-            <img src="img/others/logo_steam.svg" style="width: 80%">
-            <p>Steam to najlepsze miejsce do grania omawiania i tworzenia gier<p>
-                <span class="numbers">
-                    <span class="number">
-                        &#x2022; <span class="firstP">UŻYTKOWNICY</span><br>
-                        <?php echo $resultUser[0];?>
+    <div class="about_background">
+        <div class="about_container">
+            <div class="about_content">
+                <img src="img/others/logo_steam.svg" style="width: 80%">
+                <p>Steam to najlepsze miejsce do grania omawiania i tworzenia gier<p>
+                    <span class="numbers">
+                        <span class="number">
+                            &#x2022; <span class="firstP">UŻYTKOWNICY</span><br>
+                            <?php echo $resultUser[0];?>
+                        </span>
+                        <span class="number">
+                            &#x2022; <span class="firstP">GRY</span><br>
+                            <?php echo $resultGames[0];?>
+                        </span>
                     </span>
-                    <span class="number">
-                        &#x2022; <span class="firstP">GRY</span><br>
-                        <?php echo $resultGames[0];?>
-                    </span>
-                </span>
-            <a href="https://github.com/Kacper-Bal/ProjektSemestralny" class="btn">ZAINSTALUJ STEAM</a>
+                <a href="https://github.com/Kacper-Bal/ProjektSemestralny" class="btn">ZAINSTALUJ STEAM</a>
+            </div>
+            <div class="about_video">
+                <video width="100%" height="auto" autoplay="" muted="" loop="" playsinline="">
+                    <source src="img/others/about_information.mp4" type="video/mp4">
+                </video>
+                <div id="about_video_gradient"></div>
+            </div> 
         </div>
-        <div class="about_video">
-            <video width="100%" height="auto" autoplay="" muted="" loop="" playsinline="">
-                <source src="img/others/about_information.mp4" type="video/mp4">
-            </video>
-            <div id="about_video_gradient"></div>
-        </div> 
     </div>
     
     <div id="csg_container">
